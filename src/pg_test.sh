391a43c9be0a7a399ca61dace19aaa2cfeea77d3
ChauNgo1106 <59751996+ChauNgo1106@users.noreply.github.com>
2021-03-17 21:55:46 -0400

    Delete pg_test.sh

--- src/pg_test.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh -v
-
-time java edu.umb.cs681.primes.PGTest 1 2000000 1
-time java edu.umb.cs681.primes.PGTest 1 2000000 2
-time java edu.umb.cs681.primes.PGTest 1 2000000 4
-time java edu.umb.cs681.primes.PGTest 1 2000000 8
-time java edu.umb.cs681.primes.PGTest 1 2000000 16
